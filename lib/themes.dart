e6ac0158f7d8e42bdf72b955b2e55241950f8acc
arez <k.nikolaev@opinionsn.com>
2020-12-29 05:51:14 +0300

    Updated themes.dart

--- lib/themes.dart
+++ lib/themes.dart
@@ -8,3 +8,55 @@ class AppButtonTheme {
       letterSpacing: 1,
       fontWeight: FontWeight.w700);
 }
+
+class AppTextTheme {
+  static const xlTitle = TextStyle(
+      color: Colors.black,
+      fontFamily: 'OpenSans',
+      fontSize: 21,
+      letterSpacing: 1,
+      fontWeight: FontWeight.w700);
+
+  static const xlText =
+      TextStyle(color: Colors.black, fontFamily: 'OpenSans', fontSize: 20);
+
+  static const lgTitle = TextStyle(
+      color: Colors.black,
+      fontFamily: 'OpenSans',
+      fontSize: 19,
+      letterSpacing: 1,
+      fontWeight: FontWeight.w700);
+
+  static const lgText =
+      TextStyle(color: Colors.black, fontFamily: 'OpenSans', fontSize: 18);
+
+  static const mdTitle = TextStyle(
+      color: Colors.black,
+      fontFamily: 'OpenSans',
+      fontSize: 17,
+      letterSpacing: 1,
+      fontWeight: FontWeight.w700);
+
+  static const mdText =
+      TextStyle(color: Colors.black, fontFamily: 'OpenSans', fontSize: 16);
+
+  static const smTitle = TextStyle(
+      color: Colors.black,
+      fontFamily: 'OpenSans',
+      fontSize: 15,
+      letterSpacing: 1,
+      fontWeight: FontWeight.w700);
+
+  static const smText =
+      TextStyle(color: Colors.black, fontFamily: 'OpenSans', fontSize: 14);
+
+  static const xsTitle = TextStyle(
+      color: Colors.black,
+      fontFamily: 'OpenSans',
+      fontSize: 13,
+      letterSpacing: 1,
+      fontWeight: FontWeight.w700);
+
+  static const xsText =
+      TextStyle(color: Colors.black, fontFamily: 'OpenSans', fontSize: 12);
+}
