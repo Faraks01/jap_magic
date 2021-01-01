84dd416ae6216ab7474d2a941c52db9bd857a342
arez <k.nikolaev@opinionsn.com>
2021-01-01 22:07:05 +0300

    Removed 'hero' animation from BrandPage.dart

--- lib/pages/BrandPage.dart
+++ lib/pages/BrandPage.dart
@@ -20,7 +20,7 @@ class BrandPage extends StatelessWidget {
 
   final Brand brand;
 
-  BrandPage({this.brand});
+  BrandPage({@required this.brand});
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +33,10 @@ class BrandPage extends StatelessWidget {
         itemAspectRatio: 0.6,
         queryParams: {'brand': '${brand.id}'},
         headerWidgets: [
-          Hero(
-            tag: 'brand_picture_${brand.id}',
-            child: Image.network(
-              brand.image,
-              height: 200,
-              fit: BoxFit.contain,
-            ),
+          Image.network(
+            brand.image,
+            height: 200,
+            fit: BoxFit.contain,
           )
         ],
         modelJsonConstructor: (json) => Product.fromJson(json),
