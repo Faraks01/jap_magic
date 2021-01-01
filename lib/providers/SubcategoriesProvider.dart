e612a60f0563967a8ccc80ec8f21c1e739488541
arez <k.nikolaev@opinionsn.com>
2021-01-01 22:04:39 +0300

    Updated SubcategoriesProvider.dart

--- lib/providers/SubcategoriesProvider.dart
+++ lib/providers/SubcategoriesProvider.dart
@@ -1,4 +1,7 @@
 import 'package:jap_magic/models/Subcategory.dart';
+import 'package:jap_magic/network/ExternalApi.dart';
 import 'package:jap_magic/providers/BaseProvider.dart';
 
-class SubcategoriesProvider extends BaseProvider<Subcategory> {}
+class SubcategoriesProvider extends BaseProvider<Subcategory> {
+  final viewSet = ViewSets.subcategories;
+}
