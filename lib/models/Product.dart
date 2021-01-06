4651dec73d59865790dfd978ad7ea8d21f59e080
arez <k.nikolaev@opinionsn.com>
2021-01-06 11:02:11 +0300

    Updated Product model

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -8,25 +8,45 @@ class Product extends BaseModel {
   // @JsonKey(name: 'example_field')
   final int id;
   final List<String> tag;
+
+  @JsonKey(name: 'rating_score')
+  final double ratingScore;
+
+  @JsonKey(name: 'amount_of_feedbacks')
+  final double amountOfFeedbacks;
+
   final String name;
   final String image;
+
+  @JsonKey(name: 'description_title')
+  final double descriptionTitle;
+
   final String description;
   final String barcode;
   final String price;
   final int brand;
+  final List<int> properties;
+
+  @JsonKey(name: 'similar_products')
+  final List<int> similarProducts;
+
   final List<int> subcategories;
 
-  Product({
-    this.id,
-    this.tag,
-    this.name,
-    this.image,
-    this.description,
-    this.barcode,
-    this.price,
-    this.brand,
-    this.subcategories,
-  });
+  Product(
+      {this.id,
+      this.tag,
+      this.ratingScore,
+      this.amountOfFeedbacks,
+      this.name,
+      this.image,
+      this.descriptionTitle,
+      this.description,
+      this.barcode,
+      this.price,
+      this.brand,
+      this.properties,
+      this.similarProducts,
+      this.subcategories});
 
   get intPrice {
     return double.parse(price).toInt();
--- lib/models/Product.g.dart
+++ lib/models/Product.g.dart
@@ -10,12 +10,18 @@ Product _$ProductFromJson(Map<String, dynamic> json) {
   return Product(
     id: json['id'] as int,
     tag: (json['tag'] as List)?.map((e) => e as String)?.toList(),
+    ratingScore: (json['rating_score'] as num)?.toDouble(),
+    amountOfFeedbacks: (json['amount_of_feedbacks'] as num)?.toDouble(),
     name: json['name'] as String,
     image: json['image'] as String,
+    descriptionTitle: (json['description_title'] as num)?.toDouble(),
     description: json['description'] as String,
     barcode: json['barcode'] as String,
     price: json['price'] as String,
     brand: json['brand'] as int,
+    properties: (json['properties'] as List)?.map((e) => e as int)?.toList(),
+    similarProducts:
+        (json['similar_products'] as List)?.map((e) => e as int)?.toList(),
     subcategories:
         (json['subcategories'] as List)?.map((e) => e as int)?.toList(),
   );
@@ -24,11 +30,16 @@ Product _$ProductFromJson(Map<String, dynamic> json) {
 Map<String, dynamic> _$ProductToJson(Product instance) => <String, dynamic>{
       'id': instance.id,
       'tag': instance.tag,
+      'rating_score': instance.ratingScore,
+      'amount_of_feedbacks': instance.amountOfFeedbacks,
       'name': instance.name,
       'image': instance.image,
+      'description_title': instance.descriptionTitle,
       'description': instance.description,
       'barcode': instance.barcode,
       'price': instance.price,
       'brand': instance.brand,
+      'properties': instance.properties,
+      'similar_products': instance.similarProducts,
       'subcategories': instance.subcategories,
     };
