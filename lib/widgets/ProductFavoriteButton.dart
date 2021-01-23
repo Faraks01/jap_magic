1ddeac03d7246d22a431018bf5469f982723f4c2
arez <k.nikolaev@opinionsn.com>
2021-01-23 14:11:26 +0300

    Updated ProductFavoriteButton.dart

--- lib/widgets/ProductFavoriteButton.dart
+++ lib/widgets/ProductFavoriteButton.dart
@@ -6,12 +6,14 @@ import 'package:provider/provider.dart';
 class ProductFavoriteButton extends StatelessWidget {
   final int productId;
   final double iconSize;
+  final EdgeInsets padding;
 
-  const ProductFavoriteButton({
-    Key key,
-    this.productId,
-    this.iconSize = 34,
-  }) : super(key: key);
+  const ProductFavoriteButton(
+      {Key key,
+      @required this.productId,
+      this.iconSize = 34,
+      this.padding = EdgeInsets.zero})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +24,13 @@ class ProductFavoriteButton extends StatelessWidget {
       color: Colors.transparent,
       child: Selector<FavoriteProductsProvider, bool>(
         selector: (_, p) => p.productIds.contains(productId),
-        builder: (_, isFavorite, __) => IconButton(
-            icon: Icon(CupertinoIcons.heart_fill),
-            iconSize: iconSize,
-            color: isFavorite ? Colors.red[500] : Colors.grey[300],
+        builder: (_, isFavorite, __) => CupertinoButton(
+            padding: padding,
+            child: Icon(
+              CupertinoIcons.heart_fill,
+              color: isFavorite ? Colors.red[500] : Colors.grey[300],
+              size: iconSize,
+            ),
             onPressed: () {
               if (isFavorite) {
                 favoriteProductsProvider.removeId(productId);
