3d2c2c9bc6cf007a40a7530383f31c34106daaed
Konstantin Nikolaev <faraks01@gmail.com>
2021-07-18 10:30:38 +0300

    Updated UsersProvider

--- lib/providers/UsersProvider.dart
+++ lib/providers/UsersProvider.dart
@@ -9,14 +9,34 @@ class UsersProvider extends BaseProvider<User> {
   User mainUser;
 
   retrieveSessionUser() async {
-    final Response response = await this.fetchApi.makeRequest();
+    final Response response = await this.fetchApi.makeRequest(customViewSet: '/authorization/get');
 
     final resBody = ExternalApi.decodeResponseBody(response);
 
-    if (resBody["results"] is List) {
-      mainUser = User.fromJson(resBody["results"][0]);
+    if (response.statusCode == 200) {
+      mainUser = User.fromJson(resBody);
     }
 
     return 200;
+  }
+
+  login({String phoneNumber, String password}) async {
+    try {
+      final Response response = await this.createApi.makeRequest(customViewSet: '/authorization/login', body: {
+        "phone": phoneNumber,
+        "password": password
+      });
+
+      if (response.statusCode == 200) {
+        final resBody = ExternalApi.decodeResponseBody(response);
+        mainUser = User.fromJson(resBody);
+        notifyListeners();
+        return null;
+      } else {
+        return response.body;
+      }
+    } catch (e) {
+      return "Неизвестная ошибка";
+    }
   }
 }
