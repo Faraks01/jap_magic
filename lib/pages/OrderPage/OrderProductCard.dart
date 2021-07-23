42b9efa039662de06edabab26940ec437daf9b50
Konstantin Nikolaev <faraks01@gmail.com>
2021-07-23 17:41:21 +0300

    Updated OrderProductCard.dart

--- lib/pages/OrderPage/OrderProductCard.dart
+++ lib/pages/OrderPage/OrderProductCard.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:jap_magic/models/Product.dart';
 import 'package:jap_magic/network/Session.dart';
-import 'package:jap_magic/pages/ProductPage.dart';
-import 'package:jap_magic/providers/BrandsProvider.dart';
 import 'package:jap_magic/providers/OrderProvider.dart';
-import 'package:jap_magic/themes.dart';
+import 'package:jap_magic/widgets/ProductCard.dart';
 import 'package:provider/provider.dart';
 
 class OrderProductCard extends StatelessWidget {
@@ -26,11 +24,6 @@ class OrderProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final orderPvd = Provider.of<OrderProvider>(context, listen: false);
-
-    final nav = Navigator.of(context, rootNavigator: true);
-
-    final brand =
-        Provider.of<BrandsProvider>(context, listen: false).map[product.brand];
 
     return Padding(
       padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
@@ -54,69 +47,7 @@ class OrderProductCard extends StatelessWidget {
               });
             }
 
-            return CupertinoButton(
-              padding: EdgeInsets.zero,
-              minSize: 30,
-              onPressed: () {
-                nav.pushNamed(ProductPage.routeName,
-                    arguments: ProductPageRouteArguments(id: product.id));
-              },
-              child: Card(
-                margin: EdgeInsets.zero,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 16),
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(
-                            maxWidth: 120,
-                          ),
-                          child: Image.network(product.image, fit: BoxFit.fill),
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(brand.name, style: AppTextTheme.mdTitle),
-                            const SizedBox(height: 6),
-                            Text(product.name, style: AppTextTheme.mdText),
-                            const SizedBox(height: 3),
-                            ...product.propertiesPairs
-                                .map((e) => Padding(
-                                      padding: const EdgeInsets.only(top: 3),
-                                      child: Text.rich(TextSpan(
-                                        text: "${e[0]}: ",
-                                        style: AppTextTheme.xsTitle,
-                                        children: [
-                                          TextSpan(
-                                              text: e[1],
-                                              style: AppTextTheme.xsText.merge(
-                                                  TextStyle(
-                                                      color: Colors.grey[600])))
-                                        ],
-                                      )),
-                                    ))
-                                .toList(),
-                            const SizedBox(height: 6),
-                            Text('${product.intPrice} ₽',
-                                style: AppTextTheme.lgTitle),
-                            const SizedBox(height: 6),
-                            Text('# ${product.barcode}',
-                                style: AppTextTheme.smText),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            );
+            return ProductCard(product: product);
           },
         ),
         secondaryActions: <Widget>[
